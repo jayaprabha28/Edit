ac95ea884a44b431a32a6b6ea5c58d86f98dc411
Jayaprabha <141986095+jayaprabha28@users.noreply.github.com>
2024-10-04 13:25:58 +0530

    Add files via upload

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name : "jayaprabha",
+        email: "jayaprabha28@gmail.com",
+        id: 1
+    },
+    {
+        name : "ragul",
+        email: "ragul15@gmail.com",
+        id: 2
+    },
+    {
+        name : "vishnu",
+        email: "vishnu11@gmail.com",
+        id: 3
+    },
+]
